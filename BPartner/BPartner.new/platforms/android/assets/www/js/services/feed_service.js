b53b42aafe7af0265b677f96a738f86aa40a05a8
Le Minh Duc <minhducle.1995@outlook.com>
2016-03-26 21:33:31 +0700

    hoàn thiện chức năng chat

---
+++ BPartner/BPartner.new/platforms/android/assets/www/js/services/feed_service.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('services')
+        .service('FeedService', FeedService);
+
+
+    function FeedService() {
+
+        var feedTemp = [];
+        var senderTemp = { id: 1, name: 'Nguyễn văn A', avatar: 'http://www.app.iflipd.com/images/avatar.jpg' };
+        
+        var services = {};
+        for (var i = 1; i < 50; i++) {
+            feedTemp.push({
+                status: 'Đây là status',
+                sender: senderTemp,
+                avatar: 'http://www.app.iflipd.com/images/avatar.jpg',
+                time: '19:30',
+                commentCount: 2,
+                percent: 50
+            })
+        }
+        services.feeds = feedTemp;
+
+        services.getFeed = getFeed;
+        services.addFeed = addFeed;
+        services.editFeed = editFeed;
+        services.removeFeed = removeFeed;
+        function getFeed(id) {
+
+        }
+        function addFeed(messageText) {
+
+        }
+        function editFeed(messageText) {
+
+        }
+        function removeFeed(messageText) {
+
+        }
+        return services;
+    }
+
+
+})();
