215cb26a02caeeeed07742e9726a305072be4414
Le Minh Duc <minhducle.1995@outlook.com>
2016-03-26 21:51:48 +0700

    newFeed

---
+++ BPartner/BPartner.new/platforms/android/assets/www/js/controllers/newfeedCtrl.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+    angular.module('controllers')
+    .controller('feedCtrl', feedCtrl);
+
+    feedCtrl.$inject = ['$scope', 'FeedService'];
+
+    function feedCtrl($scope, FeedService) {
+    }
+})();
